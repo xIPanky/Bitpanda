be779ef129cfd3655749a8b9af2520ac59460b05
yannik <yannik@panke-management.com>
2026-02-23 17:44:33 +0000

    File changes

--- src/pages/OrganizerRegistration.jsx
+++ src/pages/OrganizerRegistration.jsx
@@ -14,6 +14,7 @@ export default function OrganizerRegistration() {
     password: '',
     company: '',
   });
+  const [error, setError] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
