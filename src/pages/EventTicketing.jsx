93dee3816b4abd3ff0ac10cbe8bddabdae5c1cfa
yannik <yannik@panke-management.com>
2026-02-23 21:25:42 +0000

    File changes

--- src/pages/EventTicketing.jsx
+++ src/pages/EventTicketing.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { base44 } from "@/api/base44Client";
 import { createPageUrl } from "@/utils";
